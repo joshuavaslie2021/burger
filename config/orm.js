4c4274c691c63a0d9bdda97cc62835e7a03d6d5e
joshuavaslie2021 <joshuavaslie2021@u.northwestern.edu>
2021-01-10 23:45:03 -0600

    updating orm and mysql methods

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,37 @@
-require('./connection')
+var connection = require('./connection')
+
+
+
+
+
+// Object for all our SQL statement functions.
+var orm = {
+    all: function(tableInput, cb) {
+      var queryString = "SELECT * FROM " + tableInput + ";";
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+    create: function(table, cols, vals, cb) {
+      var queryString = "INSERT INTO " + table;
+  
+      queryString += " (";
+      queryString += cols.toString();
+      queryString += ") ";
+      queryString += "VALUES (";
+      queryString += printQuestionMarks(vals.length);
+      queryString += ") ";
+  
+      console.log(queryString);
+  
+      connection.query(queryString, vals, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    },
