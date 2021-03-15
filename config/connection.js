2b18e2db64e0feca67710eda4d770cd67dbc1829
joshuavaslie2021 <joshuavaslie2021@u.northwestern.edu>
2021-03-15 01:28:07 -0500

    added jaws db to config

--- config/connection.js
+++ config/connection.js
@@ -8,11 +8,11 @@ if(process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL)
 }
 else{connection = mysql.createConnection({
-  host: "localhost",
+  host: "td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "Password1",
-  database: "burgers_db"
+  user: "j9jiru66jhmy8t1t",
+  password: "x7euixyttz76eq97",
+  database: "y99cgjb4yv1ayqg2"
 });
 }
 
